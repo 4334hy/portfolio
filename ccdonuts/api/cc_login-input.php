62ed8d955549509d53b4bde072bac2d897ca926e
4334hy <157333042+4334hy@users.noreply.github.com>
2024-03-18 13:47:55 +0900

    Add files via upload

---
+++ ccdonuts/api/cc_login-input.php
@@ -0,0 +1,17 @@
+<?php require 'header.php'; ?>
+<p>ログイン</p>
+<form action="login-output.php" method="post">
+    <p>メールアドレス</br>
+        <input type="email" name="mailaddress"><br>
+        パスワード<br>
+        <input type="password" name="password">
+    </pr>
+    <pr>
+        <input type="re" value="ログインする">
+    </pr>
+</form>
+<div class="register>
+    <a href="#formArea">会員登録はこちら</a>
+</div>
+
+<?php require 'footer.php'; ?>
